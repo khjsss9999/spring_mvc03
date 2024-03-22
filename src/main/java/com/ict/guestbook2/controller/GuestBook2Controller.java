a326adf25e2bc0f9b86ada1b1ebdd2773c79e5f9
khjsss999 <khjsss999@naver.com>
2024-03-22 18:34:20 +0900

    20240322

---
+++ src/main/java/com/ict/guestbook2/controller/GuestBook2Controller.java
@@ -0,0 +1,10 @@
+package com.ict.guestbook2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GuestBook2Controller {
+	
+	
+}
